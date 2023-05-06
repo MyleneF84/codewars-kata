f6a4ff462eaadfbe5b847237c942aeb19d7985bc
Mylene Febrissy <m.febrissy@gmail.com>
2023-05-07 00:58:08 +0200

    some 5s

---
+++ 5kyu/pig_it.rb
@@ -0,0 +1,10 @@
+# DESCRIPTION:
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+# Examples
+# pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+# pig_it('Hello world !')     # elloHay orldway !
+
+def pig_it text
+  text.gsub(/(\w+)/) { "#{$1[1..-1]}#{$1[0]}ay" }
+end
